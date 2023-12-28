32e8fd41631e64f03024f353b7d20ab19b0ad25f
PowerShell Breakfast <58199755+powershellbreakfast@users.noreply.github.com>
2023-12-28 12:03:08 -0500

    Update ?

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,7 @@
 vim.wo.number = true
 vim.wo.relativenumber = true
-
+vim.opt["tabstop"] = 4
+vim.opt["shiftwidth"] = 4
 
 require("plugins")
 
@@ -14,11 +15,14 @@ require("mason").setup({
     }
 })
 require("mason-lspconfig").setup {
-    ensure_installed = { "lua_ls", "pylsp","clangd","powershell_es" },
+    ensure_installed = { "lua_ls", "pyright","clangd","powershell_es","biome" },
 }
 
 require("lspconfig").lua_ls.setup {}
-require("lspconfig").pylsp.setup {}
+require("lspconfig").pyright.setup ({
+	filetypes = {"python"}
+})
 require("lspconfig").clangd.setup {}
 require("lspconfig").powershell_es.setup {}
+require("lspconfig").biome.setup {}
 
